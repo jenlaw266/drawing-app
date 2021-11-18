5545bad49d8f9d6e8dbc45bed001285140789f54
Jennifer Law <jenniferlaw@Jennifers-MacBook-Air-2.local>
2021-11-18 00:28:21 -0800

    set-up for drawing page

--- my-app/src/pages/Draw.js
+++ my-app/src/pages/Draw.js
@@ -1,9 +1,35 @@
-import React from "react";
+import "./Draw.css";
 
 const Draw = () => {
   return (
     <div>
-      <h1>draw page</h1>
+      <section class="section">
+        <div class="container">
+          <div class="columns">
+            <div class="column is-8">
+              <canvas id="paint"></canvas>
+              Deserunt proident nulla adipisicing pariatur adipisicing velit est
+              ut anim labore ea veniam est. Lorem veniam nisi ex aute nulla elit
+              laborum adipisicing minim id. In magna ipsum adipisicing tempor
+              laboris dolore aliquip officia cupidatat fugiat eiusmod anim.
+              Adipisicing amet tempor consectetur officia reprehenderit ipsum
+              laboris. Amet veniam velit duis magna qui. Eiusmod proident
+              pariatur occaecat non magna. Eiusmod dolore dolor laboris dolor
+              officia nulla ea nostrud.
+            </div>
+            <div class="column is-4">
+              Sint aute non do incididunt velit eiusmod magna consectetur.
+              Exercitation aliqua nulla Lorem ipsum reprehenderit magna veniam
+              officia irure et anim consectetur. Lorem nostrud laboris ullamco
+              et velit irure cillum qui laborum proident. Velit culpa laboris in
+              officia ad do aliquip non enim est irure. Aute deserunt ut id
+              labore esse veniam ut. Do amet cupidatat consequat ad in Lorem
+              nostrud magna cillum adipisicing ea do deserunt. Officia ad
+              consectetur deserunt amet minim adipisicing ex duis.
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -1,4 +1,3 @@
-import React from "react";
 import screenShot from "./temp-pic.png";
 import { Link } from "react-router-dom";
 
