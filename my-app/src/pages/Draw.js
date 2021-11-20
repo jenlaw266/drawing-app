094369f6fde9387de7f1dadecef70516c55d06c7
Jennifer Law <jenniferlaw@Jennifers-MacBook-Air-2.local>
2021-11-19 19:31:27 -0800

    minor changes

--- my-app/src/pages/Draw.js
+++ my-app/src/pages/Draw.js
@@ -3,21 +3,17 @@ import "./Draw.css";
 const Draw = () => {
   return (
     <div>
-      <section class="section">
-        <div class="container">
-          <div class="columns">
-            <div class="column is-8">
-              <canvas id="paint"></canvas>
-              Deserunt proident nulla adipisicing pariatur adipisicing velit est
-              ut anim labore ea veniam est. Lorem veniam nisi ex aute nulla elit
-              laborum adipisicing minim id. In magna ipsum adipisicing tempor
-              laboris dolore aliquip officia cupidatat fugiat eiusmod anim.
-              Adipisicing amet tempor consectetur officia reprehenderit ipsum
-              laboris. Amet veniam velit duis magna qui. Eiusmod proident
-              pariatur occaecat non magna. Eiusmod dolore dolor laboris dolor
-              officia nulla ea nostrud.
+      <section className="section">
+        <div className="container">
+          <div className="columns">
+            <div className="column is-8">
+              <canvas
+                id="paint"
+                onClick={(e) => console.log(e.pageX, e.pageY)}
+              ></canvas>
             </div>
-            <div class="column is-4">
+            <div className="column is-4">
+              <br />
               Sint aute non do incididunt velit eiusmod magna consectetur.
               Exercitation aliqua nulla Lorem ipsum reprehenderit magna veniam
               officia irure et anim consectetur. Lorem nostrud laboris ullamco
